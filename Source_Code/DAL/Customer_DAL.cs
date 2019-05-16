5d070c6d4dca7c35592aa72bd25e8c33208627be
AnhLinhVTCA <linhna.nde18090@vtc.edu.vn>
2019-05-16 10:50:57 +0700

    dbhelper

---
+++ Source_Code/DAL/Customer_DAL.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Text.RegularExpressions;
+using CTS_Persistence;
+using MySql.Data.MySqlClient;
+
+namespace DAL
+{
+    public class Customer_DAL
+    {
+        private static MySqlConnection connection;
+    }
+}
--- Source_Code/DAL/DbHelper.cs
+++ Source_Code/DAL/DbHelper.cs
@@ -1,8 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
 
 namespace DAL
 {
     public class DbHelper
     {
+        private static string connection_String = "server=localhost;user id=root;password=Hanhphuc1;port=3306;database=headphone_shop_gr3;SslMode=None";
+        public static MySqlConnection OpenDefaultConnection()
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection
+                {
+                    ConnectionString = connection_String
+                };
+                connection.open();
+                return connection;
+            }
+            catch (System.Exception)
+            {
+
+                return null;
+            }
+        }
+
+        public static MySqlConnection OpenConnection()
+        {
+            try
+            {
+                string connectionString;
+                FileStream fileStream = File.OpenRead("ConnectionString.txt");
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    connectionString = reader.ReadLine();
+                }
+                fileStream.Close();
+                return OpenConnection(connectionString);
+            }
+            catch (System.Exception)
+            {
+
+                return null;
+            }
+        }
+
+        public static MySqlConnection OpenConnection(string connectionString)
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection
+                {
+                    ConnectionString = connectionString
+                };
+                connection.open();
+                return connection;
+            }
+            catch (System.Exception)
+            {
+
+                return null;
+            }
+        }
     }
 }
